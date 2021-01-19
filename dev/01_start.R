f249310488f59344d747246dc2b29d9c075358c8
TracyRage <robohoratio@gmail.com>
2021-01-19 09:58:35 +0200

    Initial commit

---
+++ dev/01_start.R
@@ -0,0 +1,64 @@
+# Building a Prod-Ready, Robust Shiny Application.
+# 
+# README: each step of the dev files is optional, and you don't have to 
+# fill every dev scripts before getting started. 
+# 01_start.R should be filled at start. 
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
+# 
+# 
+########################################
+#### CURRENT FILE: ON START SCRIPT #####
+########################################
+
+## Fill the DESCRIPTION ----
+## Add meta data about your application
+golem::fill_desc(
+  pkg_name = "muddy", # The Name of the package containing the App 
+  pkg_title = "muddy_db", # The Title of the package containing the App 
+  pkg_description = "App muddy_db represents a biologically-oriented mud
+  volcano database. It aggregates mud volcano specific terminology and taxonomy, 
+  which were mined from open-access articles available in the S2ORC database.
+  In order to extract tokens of interest, we used scispaCy models and NCBI
+  Taxonomy database.", # The Description of the package containing the App 
+  author_first_name = "Alexei", # Your First Name
+  author_last_name = "Remizovschi", # Your Last Name
+  author_email = "alexei.remizovschi@protonmail.ch", # Your Email
+  repo_url = "https://github.com/TracyRage/muddy_db" # The URL of the GitHub Repo (optional) 
+)     
+
+## Set {golem} options ----
+golem::set_golem_options()
+
+## Create Common Files ----
+## See ?usethis for more information
+usethis::use_mit_license( "Alexei Remizovschi" )  # You can set another license here
+usethis::use_readme_rmd( open = FALSE )
+usethis::use_code_of_conduct()
+usethis::use_lifecycle_badge( "Experimental" )
+usethis::use_news_md( open = FALSE )
+
+## Use git ----
+usethis::use_git()
+
+## Init Testing Infrastructure ----
+## Create a template for tests
+golem::use_recommended_tests()
+
+## Use Recommended Packages ----
+golem::use_recommended_deps()
+
+## Favicon ----
+# If you want to change the favicon (default is golem's one)
+golem::remove_favicon()
+golem::use_favicon() # path = "path/to/ico". Can be an online file. 
+
+## Add helper functions ----
+golem::use_utils_ui()
+golem::use_utils_server()
+
+# You're now set! ----
+
+# go to dev/02_dev.R
+rstudioapi::navigateToFile( "dev/02_dev.R" )
+
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(muddy)
+
+test_check("muddy")
