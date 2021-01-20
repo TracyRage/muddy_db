c7b262ba288530137f16b8a770bb29a35ddc56f6
TracyRage <robohoratio@gmail.com>
2021-01-21 00:18:50 +0200

    Removed: golem_external_resources

--- R/app_ui.R
+++ R/app_ui.R
@@ -8,7 +8,7 @@
 app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
-    golem_add_external_resources(),
+    # golem_add_external_resources(),
     # List the first level UI elements here 
     # Create dashboard sidebar bars
     dashboard_tabs <- sidebarMenu(
@@ -83,28 +83,28 @@ app_ui <- function(request) {
                 dashboard_body)
 }
 
-#' Add external Resources to the Application
-#' 
-#' This function is internally used to add external 
-#' resources inside the Shiny application. 
-#' 
-#' @import shiny
-#' @importFrom golem add_resource_path activate_js favicon bundle_resources
-#' @noRd
-golem_add_external_resources <- function(){
-  
-  add_resource_path(
-    'www', app_sys('app/www')
-  )
- 
-  tags$head(
-    favicon(),
-    bundle_resources(
-      path = app_sys('app/www'),
-      app_title = 'muddy'
-    )
-    # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
-  )
-}
+#' #' Add external Resources to the Application
+#' #' 
+#' #' This function is internally used to add external 
+#' #' resources inside the Shiny application. 
+#' #' 
+#' #' @import shiny
+#' #' @importFrom golem add_resource_path activate_js favicon bundle_resources
+#' #' @noRd
+#' golem_add_external_resources <- function(){
+#'   
+#'   add_resource_path(
+#'     'www', app_sys('app/www')
+#'   )
+#'  
+#'   tags$head(
+#'     favicon(),
+#'     bundle_resources(
+#'       path = app_sys('app/www'),
+#'       app_title = 'muddy'
+#'     )
+#'     # Add here other external resources
+#'     # for example, you can add shinyalert::useShinyalert() 
+#'   )
+#' }
 
